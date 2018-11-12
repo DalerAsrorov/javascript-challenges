548fe26ddf6ed182fe936f2f3e7e7bfe6891c487
Daler Asrorov <asrorids@gmail.com>
2018-11-12 03:29:54 -0800

    Added answer to balenced-parens

--- balanced-parens/balanced-parens.js
+++ balanced-parens/balanced-parens.js
@@ -1,5 +1,29 @@
-const parensAreBalanced = (input) => {
-  return false
-}
+let PARENS_MAPPING = {
+  '[': ']',
+  '(': ')',
+  '{': '}'
+};
 
-module.exports = parensAreBalanced
+const parensAreBalanced = input => {
+  let stack = [];
+
+  for (let i = 0; i < input.length; i++) {
+    let char = input[i];
+
+    if (char === '(' || char === '[' || char === '{') {
+      stack.push(char);
+    } else if (char === ')' || char === ']' || char === '}') {
+      let popped = stack[stack.length - 1] ? stack.pop() : '#';
+
+      if (char !== PARENS_MAPPING[popped]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
+
+let result = parensAreBalanced('{}');
+
+module.exports = parensAreBalanced;
