a1c3498c6837a4f7eba91f5c94f8673ea91a5594
Daler Asrorov <asrorids@gmail.com>
2018-11-12 02:56:02 -0800

    add .gitignore

--- babylonian-method/babylonian.js
+++ babylonian-method/babylonian.js
@@ -1,5 +1,26 @@
-const squareRoot = (radicand) => {
-  return radicand
-}
+const floor = Math.floor;
 
-module.exports = squareRoot
+const guess = x => x / 2;
+
+const squareRoot = radicand => {
+  if (typeof radicand !== 'number' || radicand < 0) {
+    return NaN;
+  }
+  if (radicand === 0) {
+    return 0;
+  }
+
+  let x = radicand;
+  let y = 1;
+
+  while (x - y > 0.0) {
+    x = (x + y) / 2;
+    y = radicand / x;
+  }
+
+  return x;
+};
+
+console.log(squareRoot(49));
+
+module.exports = squareRoot;
