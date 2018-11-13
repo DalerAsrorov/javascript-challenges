fb30caf14b24c1100aeb91ab3931be8aa3c32c72
Daler Asrorov <asrorids@gmail.com>
2018-11-13 08:15:59 -0800

    Another approach to set timeout

---
+++ recreate-setinterval/solution2.js
@@ -0,0 +1,41 @@
+const func = d => console.log('hi', d);
+
+let mySetInterval = function(fn, time) {
+  mySetInterval.intervals = mySetInterval.intervals
+    ? mySetInterval.intervals
+    : new Map();
+  mySetInterval.id = mySetInterval.id ? mySetInterval.id + 1 : 1;
+
+  let intervalsMap = mySetInterval.intervals;
+
+  intervalsMap.set(
+    this.id,
+    setTimeout(function next() {
+      intervalsMap.set(this.id, setTimeout(next, time));
+      fn.apply(this, arguments);
+    }, time)
+  );
+
+  return this.id;
+};
+
+function clearInterval(id) {
+  clearTimeout(mySetInterval.intervals.get(id));
+}
+
+this.interval1 = mySetInterval(() => {
+  clearInterval(this.interval1);
+  func('daler');
+}, 5000);
+this.interval2 = mySetInterval(() => {
+  clearInterval(this.interval2);
+  func('allan');
+}, 5000);
+this.interval3 = mySetInterval(() => {
+  //   clearInterval(this.interval3);
+  func('dj');
+}, 5000);
+
+// this.interval2 = mySetInterval(() => {
+//   func('daler');
+// }, 5000);
