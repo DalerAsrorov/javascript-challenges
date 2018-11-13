bc1fd24b4abe2ac41cd921a90280519dd1d28045
Daler Asrorov <asrorids@gmail.com>
2018-11-13 06:31:19 -0800

    Solution to most recurrent

--- number-frequency/most-recurrent.js
+++ number-frequency/most-recurrent.js
@@ -1,5 +1,31 @@
-const mostRecurrent = (set) => {
-  return set
-}
+const mostRecurrent = set => {
+  let cache = {};
 
-module.exports = mostRecurrent
+  for (let i = 0; i < set.length; i++) {
+    let num = set[i];
+
+    if (!cache[num]) {
+      cache[num] = [1, i, num]; // (count, firstIndex, num)
+    } else {
+      cache[num][0] += 1;
+    }
+  }
+
+  const sorted = Object.values(cache).sort((a, b) => {
+    if (a[0] < b[0]) {
+      return 1;
+    } else if (a[0] > b[0]) {
+      return -1;
+    } else if (a[0] === b[0]) {
+      if (a[1] < b[1]) {
+        return -1;
+      }
+      return 1;
+    }
+    return 0;
+  });
+
+  return sorted[0][2];
+};
+
+module.exports = mostRecurrent;
