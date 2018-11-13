13821aaedec551de5d4619538e5edc6cb93f3aa2
Daler Asrorov <asrorids@gmail.com>
2018-11-13 07:32:55 -0800

    Implemented Promise.all

--- promise-dot-all/promise-dot-all.js
+++ promise-dot-all/promise-dot-all.js
@@ -1,5 +1,27 @@
-function all (promises) {
-  return {}
+function all(promises) {
+  return new Promise((res, rej) => {
+    const numberOfPromises = promises.length;
+    let priorityQueue = [];
+
+    if (numberOfPromises === 0) {
+      res(priorityQueue);
+    }
+
+    promises.forEach((p, i) => {
+      if (!(p instanceof Promise)) {
+        p = Promise.resolve(p);
+      }
+
+      p.then(d => {
+        priorityQueue.push([d, i]);
+
+        if (priorityQueue.length >= numberOfPromises) {
+          priorityQueue.sort((a, b) => a[1] - b[1]);
+          res(priorityQueue.map(a => a[0]));
+        }
+      }).catch(error => rej(error));
+    });
+  });
 }
 
-module.exports = all
+module.exports = all;
