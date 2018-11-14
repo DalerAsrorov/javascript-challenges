f25a5c3b39ecff4f878512bbe3876c06ebaa8709
Daler Asrorov <asrorids@gmail.com>
2018-11-14 04:35:19 -0800

    Improved solution of add-eventing

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,23 +1,22 @@
-// solution 1 (fails emit listener test)
 const addEventing = function(obj) {
   let eventCache = {};
 
-  return {
-    on: (eventName, callback) => {
-      if (eventCache[eventName]) {
-        eventCache[eventName] = [...eventCache[eventName], callback.bind(obj)];
-      } else {
-        eventCache[eventName] = [callback.bind(obj)];
-      }
-    },
-    trigger: (eventName, ...args) => {
-      if (eventCache[eventName]) {
-        eventCache[eventName].forEach(callbackFn => {
-          callbackFn(...args);
-        });
-      }
+  obj.on = (eventName, callback) => {
+    if (eventCache[eventName]) {
+      eventCache[eventName] = [...eventCache[eventName], callback.bind(obj)];
+    } else {
+      eventCache[eventName] = [callback.bind(obj)];
     }
   };
+  obj.trigger = (eventName, ...args) => {
+    if (eventCache[eventName]) {
+      eventCache[eventName].forEach(callbackFn => {
+        callbackFn(...args);
+      });
+    }
+  };
+
+  return obj;
 };
 
 module.exports = addEventing;
