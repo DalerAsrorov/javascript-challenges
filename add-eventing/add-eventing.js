04582e458bcdbf0eb1755aa2d589d2c6e6ee3303
Daler Asrorov <asrorids@gmail.com>
2018-11-14 04:10:05 -0800

    Added eventing solution

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,43 @@
-const addEventing = function (obj) {
-}
+const EventEmitter = require('events');
 
-module.exports = addEventing
+// solution 1 (fails emit listener test)
+// const addEventing = function(obj) {
+//   let eventCache = {};
+
+//   obj.on = (eventName, callback) => {
+//     if (eventCache[eventName]) {
+//       eventCache[eventName] = [...eventCache[eventName], callback.bind(obj)];
+//     } else {
+//       eventCache[eventName] = [callback.bind(obj)];
+//     }
+//   };
+
+//   obj.trigger = eventName => {
+//     if (eventCache[eventName]) {
+//       eventCache[eventName].forEach(callbackFn => {
+//         callbackFn();
+//       });
+//     }
+//   };
+
+//   return obj;
+// };
+
+// Using EventEmitter - passes all tests
+const addEventing = function(obj) {
+  let eventEmitter = new EventEmitter();
+
+  obj.on = (eventName, callback) => {
+    eventEmitter.on(eventName, callback);
+  };
+
+  obj.trigger = eventName => {
+    if (eventEmitter.eventNames().includes(eventName)) {
+      eventEmitter.emit(eventName);
+    }
+  };
+
+  return obj;
+};
+
+module.exports = addEventing;
