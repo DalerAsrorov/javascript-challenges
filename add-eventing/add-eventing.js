f77ce2fe14d51004be2d65f7d93456a328f09a73
Daler Asrorov <asrorids@gmail.com>
2018-11-14 04:30:52 -0800

    Solution

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,43 +1,23 @@
-const EventEmitter = require('events');
-
 // solution 1 (fails emit listener test)
-// const addEventing = function(obj) {
-//   let eventCache = {};
-
-//   obj.on = (eventName, callback) => {
-//     if (eventCache[eventName]) {
-//       eventCache[eventName] = [...eventCache[eventName], callback.bind(obj)];
-//     } else {
-//       eventCache[eventName] = [callback.bind(obj)];
-//     }
-//   };
-
-//   obj.trigger = eventName => {
-//     if (eventCache[eventName]) {
-//       eventCache[eventName].forEach(callbackFn => {
-//         callbackFn();
-//       });
-//     }
-//   };
-
-//   return obj;
-// };
-
-// Using EventEmitter - passes all tests
 const addEventing = function(obj) {
-  let eventEmitter = new EventEmitter();
+  let eventCache = {};
 
-  obj.on = (eventName, callback) => {
-    eventEmitter.on(eventName, callback);
-  };
-
-  obj.trigger = eventName => {
-    if (eventEmitter.eventNames().includes(eventName)) {
-      eventEmitter.emit(eventName);
+  return {
+    on: (eventName, callback) => {
+      if (eventCache[eventName]) {
+        eventCache[eventName] = [...eventCache[eventName], callback.bind(obj)];
+      } else {
+        eventCache[eventName] = [callback.bind(obj)];
+      }
+    },
+    trigger: (eventName, ...args) => {
+      if (eventCache[eventName]) {
+        eventCache[eventName].forEach(callbackFn => {
+          callbackFn(...args);
+        });
+      }
     }
   };
-
-  return obj;
 };
 
 module.exports = addEventing;
