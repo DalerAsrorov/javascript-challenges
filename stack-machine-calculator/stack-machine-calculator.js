13d410312ca857f8e28e19e78fd46467c1e2a184
DalerAsrorov <asrorids@gmail.com>
2020-12-01 21:29:14 -0500

    Stack machine implementation

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -1,5 +1,28 @@
-const stackMachineCalculator = (instructions) => {
-  return instructions
-}
+const getLast = (list) => !!list && list.length ? list[list.length - 1] : null;
 
-module.exports = stackMachineCalculator
+const stackMachineCalculator = (instructions) => {
+  this.stack = [];
+  const instrList = instructions.trim().split(' ');
+
+  for (let instruction of instrList) {
+    if (!isNaN(instruction)) {
+      this.stack.push(instruction);
+    } else if (instruction.toUpperCase() === 'DUP') {
+      this.stack.push(getLast(this.stack));
+    } else if (instruction.toUpperCase() === 'POP') {
+      this.stack.pop();
+    } else if (instruction === '+') {
+      let last = parseInt(this.stack.pop() || 0);
+      let secondLast = parseInt(this.stack.pop() || 0);
+      this.stack.push(last + secondLast);
+    } else if (instruction === '-') {
+      let last = parseInt(this.stack.pop() || 0);
+      let secondLast = parseInt(this.stack.pop() || 0);
+      this.stack.push(last - secondLast);
+    }
+  }
+
+  return Number(getLast(this.stack));
+};
+
+module.exports = stackMachineCalculator;
